8660a2e27c0b78af2fcb9b8ad1045c0b71f1f8b8
dachengshan <103615022+dachengshan@users.noreply.github.com>
2024-03-24 19:01:39 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -21,7 +21,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '泥瓦匠', // 您的昵称 例如 tangly102
   BIO: process.env.NEXT_PUBLIC_BIO || '一个普通的工程人', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || ', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || '', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '建筑博客', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
